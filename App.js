700f1a2bd567d16d5f16ef46121da25893519406
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-01-31 17:59:57 -0500

    styles

--- App.js
+++ App.js
@@ -21,11 +21,17 @@ export default function App() {
           placeholder="Your fourse goal!"
           onChangeText={goalInputHandler}
         />
-        <Button onPress={addGoalHandler} style={styles.button} title="Add" />
+        <View style={styles.button}>
+          <Button onPress={addGoalHandler} title="Add" color="#dbe1f4" />
+        </View>
       </View>
+
       <View style={styles.goalsContainer}>
+        <Text style={styles.title}>To Buy:</Text>
         {courseGoals.map((item) => (
-          <Text>{item}</Text>
+          <Text style={styles.item} key={item}>
+            🛒 {item}
+          </Text>
         ))}
       </View>
     </View>
@@ -35,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#141414',
     padding: 50,
     height: '100%',
   },
@@ -49,9 +55,11 @@ const styles = StyleSheet.create({
     borderBottomColor: '#cccccc',
   },
   button: {
-    backgroundColor: 'red',
+    backgroundColor: '#5169b9',
+    borderRadius: 5,
   },
   textInput: {
+    color: 'white',
     borderWidth: 1,
     borderColor: '#cccccc',
     padding: 8,
@@ -60,5 +68,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 4,
+  },
+  title: {
+    color: '#5169b9',
+    fontSize: 20,
+    fontWeight: 800,
+    paddingBottom: 10,
+  },
+  item: {
+    color: 'white',
+    padding: 3,
   },
 });
