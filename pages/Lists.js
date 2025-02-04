427e2e7e1d1cce57ddcbe99815d3586f9cbc2159
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-03 23:24:09 -0500

    lists

---
+++ pages/Lists.js
@@ -0,0 +1,7 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function Lists() {
+  return <View style={styles.appContainer}></View>;
+}
+
+const styles = StyleSheet.create({});
